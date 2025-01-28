f0e3f51cc18f852975768b9c4f8cace0755119b2
Tristan Lize <tlize@c2r7p4.42lehavre.fr>
2025-01-28 13:28:29 +0100

    added libft

---
+++ libft/ft_printf/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 14:53:28 by tlize             #+#    #+#             */
+/*   Updated: 2024/11/27 16:08:40 by tlize            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdint.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_printfc(int s);
+int		ft_printfs(char *str);
+int		ft_printfp(void *ptr);
+int		ft_printfu(unsigned int n);
+int		ft_printfx(unsigned int n, char format);
+int		ft_printfd(int n);
+
+#endif
